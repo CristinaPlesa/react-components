769b75da0557fa5d4806a3aa17d477a7b43f15d9
Cristina Plesa <cristinaplesa8@gmail.com>
2021-08-21 16:36:16 -0700

    add approval card display to all comment details

--- src/index.js
+++ src/index.js
@@ -7,22 +7,33 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-      author="Sam" 
-      timeAgo="Today at 4:45PM" 
-      content="Very nice!" 
-      avatar={faker.image.avatar()} />
-      <CommentDetail 
-      author="Alex" 
-      timeAgo="Today at 2:00AM" 
-      content="Love it!" 
-      avatar={faker.image.avatar()} />
-      <CommentDetail 
-      author="Jane" 
-      timeAgo="Yesterday at 5:00PM" 
-      content="Nice blog post!" 
-      avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45PM" 
+        content="Very nice!" 
+        avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 2:00AM" 
+        content="Love it!" 
+        avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+        author="Jane" 
+        timeAgo="Yesterday at 5:00PM" 
+        content="Nice blog post!" 
+        avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 };
