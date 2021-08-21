976924d4414aae3fb86d31a1110e9cb35c566440
Cristina Plesa <cristinaplesa8@gmail.com>
2021-08-21 15:55:02 -0700

    add reusable child component for user comments

--- src/index.js
+++ src/index.js
@@ -1,52 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-          </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-          </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-          <div className="content">
-            <a href="/" className="author">
-              Sam
-            </a>
-            <div className="metadata">
-              <span className="date">Today at 6:00PM</span>
-            </div>
-            <div className="text">Nice blog post!</div>
-          </div>
-      </div>
+      <CommentDetail 
+      author="Sam" 
+      timeAgo="Today at 4:45PM" 
+      content="Very nice!" 
+      avatar={faker.image.avatar()} />
+      <CommentDetail 
+      author="Alex" 
+      timeAgo="Today at 2:00AM" 
+      content="Love it!" 
+      avatar={faker.image.avatar()} />
+      <CommentDetail 
+      author="Jane" 
+      timeAgo="Yesterday at 5:00PM" 
+      content="Nice blog post!" 
+      avatar={faker.image.avatar()} />
     </div>
   );
 };
